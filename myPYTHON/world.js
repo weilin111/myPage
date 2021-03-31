5121f43e9455d147828d3f42f3b90132badfbb42
weilin111\sirius.github.io <49980842+weilin111@users.noreply.github.com>
2021-03-31 21:52:13 +0800

    1

---
+++ myPYTHON/world.js
@@ -0,0 +1,32 @@
+class phy_Object {
+
+
+}
+
+class static_obj {
+
+}
+
+class world {
+
+    phy_Object_list = []
+
+    static_obj = []
+
+    grabity = []
+        //  2d vector
+
+    var dt = 0.1
+
+    var t = 0
+
+    update() {
+
+    }
+
+}
+class UI {
+
+    // manipulate the DOM
+    draw()
+}
