cab05a332a136f7b99b7f6712d189d07adee91ec
Your Name <you@example.com>
2022-01-08 16:02:06 +0800

    1

---
+++ style/js/code_copy.js
@@ -0,0 +1,28 @@
+
+
+
+code_tag_list=$('code')
+// =$('code')[0].innerText
+
+
+
+
+// $($('code')[0]).parent().append('<button style="background-color:red">copy</button>')
+
+var clipJS=new ClipboardJS('.code_copy_btn');
+
+for(let i=0; i<$('code').length;i++){
+
+    $($('code')[i]).attr('id','code_'+i)
+    // $($('code')[i]).attr('class','highlight')
+    // style="style=background-color:red  "
+
+
+    id='id=bnt_code_tag_'+i+'  '
+    target="data-clipboard-target=\'#"+'code_'+i+'\' ' 
+    $($('code')[i]).parent().append('<button class="code_copy_btn"  '+ target +id + '>copy</button>')
+
+}
+
+
+
