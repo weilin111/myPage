0b400cd9d3ba40ffac09dd4dcf1867648202bdc5
Your Name <you@example.com>
2022-01-17 08:18:20 +0800

    1

--- style/js/myP5.js
+++ style/js/myP5.js
@@ -194,3 +194,117 @@ const s3 = ( sketch ) => {
 
 
 
+const s4 = ( sketch ) => {
+    let canvas;
+    let metaballShader;
+    const N_balls = 20,
+          metaballs = [];
+
+    sketch.setup = () => {
+      canvas=sketch.createCanvas(400, 400,sketch.WEBGL);
+      canvas.parent('p5js-lists');
+      sketch.pixelDensity(1)
+      sketch.shader(metaballShader);
+      for (let i = 0; i < N_balls; i ++) metaballs.push(new Metaball());    
+    };
+  
+    sketch.draw = () => {
+      sketch.background(0);
+      var data = [];
+      
+      for (const ball of metaballs) {
+        ball.update();
+        data.push(ball.pos.x, ball.pos.y, ball.radius);
+      }
+      
+      metaballShader.setUniform("metaballs", data);
+      sketch.rect(0, 0, sketch.width, sketch.height);
+    };
+
+    
+    sketch.preload=()=> {
+      metaballShader = getShader(this._renderer);
+    }
+    // OpenProcessing has a bug where it always creates a scrollbar on Chromium.
+    // sketch.mouseWheel=()=> { // This stops the canvas from scrolling by a few pixels.
+    //   return false;
+    // }
+
+    class Metaball {
+      constructor() {
+        const size = Math.pow(Math.random(), 2);
+        this.vel = p5.Vector.random2D().mult(8 * (1 - size) + 2);
+        this.radius = 25 * size + 15;
+        
+        this.pos = new p5.Vector(sketch.width / 2, sketch.height / 2);
+      }
+      
+      update() {
+        this.pos.add(this.vel);
+        
+        if (this.pos.x < this.radius || this.pos.x > sketch.width  - this.radius) this.vel.x *= -1;
+        if (this.pos.y < this.radius || this.pos.y > sketch.height - this.radius) this.vel.y *= -1;
+      }
+    }
+
+    function getShader(_renderer) {
+      const vert = `
+        attribute vec3 aPosition;
+        attribute vec2 aTexCoord;
+    
+        varying vec2 vTexCoord;
+    
+        void main() {
+          vTexCoord = aTexCoord;
+    
+          vec4 positionVec4 = vec4(aPosition, 1.0);
+          positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+    
+          gl_Position = positionVec4;
+        }
+      `;
+    
+      const frag = `
+        precision highp float;
+    
+        varying vec2 vTexCoord;
+    
+        uniform vec3 metaballs[${N_balls}];
+    
+        const float WIDTH = ${400}.0;
+        const float HEIGHT = ${400}.0;
+    
+        vec3 hsv2rgb(vec3 c) {
+            vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+            vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+            return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+        }
+    
+        void main() {
+          float x = vTexCoord.x * WIDTH;
+          float y = vTexCoord.y * HEIGHT;
+          float v = 0.0;
+    
+          for (int i = 0; i < ${N_balls}; i++) {
+            vec3 ball = metaballs[i];
+            float dx = ball.x - x;
+            float dy = ball.y - y;
+            float r = ball.z;
+            v += r * r / (dx * dx + dy * dy);
+          }
+    
+          if (0.9 < v && v < 1.1) {
+            float a = (v - 0.9) * 4.;
+            gl_FragColor = vec4(hsv2rgb(vec3(a, 1., 1.)), 1.0);
+          } else gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+      `;
+      
+      return new p5.Shader(_renderer, vert, frag);
+    }
+
+
+
+  };
+  
+  let myp5_4 = new p5(s4);
